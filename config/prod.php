73f3f823e0a60f16681c6ec67e4903cffed11506
Kacper Donat <kadet1090@gmail.com>
2021-03-14 18:39:59 +0100

    Add logs on prod

--- config/prod.php
+++ config/prod.php
@@ -1,9 +1,15 @@
 <?php
 
+use Silex\Provider\MonologServiceProvider;
+
 // configure your app for the production environment
 
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../var/logs/silex_dev.log',
+));
 
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
